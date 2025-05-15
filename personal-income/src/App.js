da0e044ab090608da0831ff97f4ad1ba5640e927
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-15 08:10:54 -0700

    created initial state

--- personal-income/src/App.js
+++ personal-income/src/App.js
@@ -1,9 +1,40 @@
 import { useReducer } from 'react'
 import { useState } from 'react'
+const initialState = {
+  expenses: [
+    { category: "Transport", amount: 0 },
+    { category: "Food", amount: 0 }
+  ],
+  balance: 0,
+  income: 0
+};
+
+interface Action{
+  type : "increment"," decrement", || "updateExpenses"
+}
+function reducer(state, action){
+  switch(action.type){
+      case"updateExpenses":
+      return{
+        ...state,
+        [action.payload]
+      }
+      default: return state;
+      switch(action.type){
+        case"increment"
+        return{
+          ...state,
+          [action.field]:Number(action.value),
+        };
+        default: return state;
+      }
+      
+  }
+}
 
 function App() {
-  const [headings, setHeadings] = useState(["Expenses", "Amount", "Balance"])
-   const [data, setData] = useState([{Expenses:"Transportation",Amount:"hhhh", Balance:"gggg"}])
+  const [headings, setHeadings] = useState(["Expenses", "Amount", "Balance", "Income"])
+
 return(
   <div>
     <table>
