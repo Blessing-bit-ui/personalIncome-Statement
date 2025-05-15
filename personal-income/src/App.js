96842d4167d005e9122f2c7bfac73e9566cc508d
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-15 06:20:24 -0700

    set heading

--- personal-income/src/App.js
+++ personal-income/src/App.js
@@ -2,7 +2,21 @@ import { useReducer } from 'react'
 import { useState } from 'react'
 
 function App() {
-  
+  const [headings, setHeadings] = useState(["Expenses", "Amount", "Balance"])
+
+return(
+  <div>
+    <table>
+      <thead>
+        <tr>
+          {headings.map((heading, index)=>(
+            <th key={index}>{heading}</th>
+          ))}
+          </tr>
+          </thead>
+    </table>
+  </div>
+)
 
 }
 export default App;
